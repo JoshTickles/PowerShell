3bb090f401341191ba637dd40d7e2b850c4975aa
JoshTickles <joshangel@hotmail.co.uk>
2019-11-11 07:52:22 +1300

    Update RestrictedDGSenders.ps1

--- Azure/RestrictedDGSenders.ps1
+++ Azure/RestrictedDGSenders.ps1
@@ -1,7 +1,9 @@
 ## Restrict Distribution group senders to Staff Only. 
-## Josh A - Sept 2017. 
+## Josh A - Sept 2017 - Last update - June 2019
 ## This script simply restricts distribution group sending to allowed users only.
 ## Uses - Stopping users from emailing any dist group such as class groups or 'all-staff' etc.
+##
+## This is super dirty, and janky, and needs to be tidied with loops when poss.
 
 ## ---------- Variables
 # Change the below as required. 
@@ -10,6 +12,8 @@
 $SearchBase ='OU=Groups,OU=OU2,DC=DOAMIN,DC=nz'
 # Your security group that contains allowed senders.
 $AllowedUsers = "CN=Perm-AllowedToSendToDistributionLists,OU=Office365 Groups,OU=Groups,OU=OU1,DC=DOMAIN,DC=nz"
+# NUll sender group
+$NULLUsers = "CN=Perm-NULLSending,OU=Office365 Groups,OU=Groups,OU=OU1,DC=DOMAIN,DC=nz"
 
 ## ---------- Start
 
@@ -18,3 +22,35 @@ Foreach ($group in (Get-ADGroup -Filter * -SearchBase $SearchBase)) {
   Set-ADGroup $group.DistinguishedName -add @{msExchRequireAuthToSendTo=$true}
   Set-ADGroup $group.DistinguishedName -add @{dlMemSubmitPerms=$AllowedUsers}
   }
+
+## ---------- Groups to hide.
+
+  Set-ADGroup "Subject 09AMP" -add @{msExchHideFromAddressLists=$true}
+  Set-ADGroup "Subject 10AMP" -add @{msExchHideFromAddressLists=$true} 
+  Set-ADGroup "Subject 11AMP" -add @{msExchHideFromAddressLists=$true}
+  Set-ADGroup "Subject 12AMP" -add @{msExchHideFromAddressLists=$true} 
+  Set-ADGroup "Subject 13AMP" -add @{msExchHideFromAddressLists=$true}
+
+#Now Remove permissions to global Form Classes - We have to do this so that no one emails these groups by accident. Groups still needed at the moment. 
+  $group="Subject 09AMP"
+  Set-ADGroup $group -add @{msExchRequireAuthToSendTo=$true}
+  Set-ADGroup $group -remove @{dlMemSubmitPerms=$AllowedUsers}
+  Set-ADGroup $group -add @{dlMemSubmitPerms=$NULLUsers}
+  $group="Subject 10AMP"
+  Set-ADGroup $group -add @{msExchRequireAuthToSendTo=$true}
+  Set-ADGroup $group -remove @{dlMemSubmitPerms=$AllowedUsers}
+  Set-ADGroup $group -add @{dlMemSubmitPerms=$NULLUsers}
+  $group="Subject 11AMP"
+  Set-ADGroup $group -add @{msExchRequireAuthToSendTo=$true}
+  Set-ADGroup $group -remove @{dlMemSubmitPerms=$AllowedUsers}
+  Set-ADGroup $group -add @{dlMemSubmitPerms=$NULLUsers}
+  $group="Subject 12AMP"
+  Set-ADGroup $group -add @{msExchRequireAuthToSendTo=$true}
+  Set-ADGroup $group -remove @{dlMemSubmitPerms=$AllowedUsers}
+  Set-ADGroup $group -add @{dlMemSubmitPerms=$NULLUsers}
+  $group="Subject 13AMP"
+  Set-ADGroup $group -add @{msExchRequireAuthToSendTo=$true}
+  Set-ADGroup $group -remove @{dlMemSubmitPerms=$AllowedUsers}
+  Set-ADGroup $group -add @{dlMemSubmitPerms=$NULLUsers}
+  
+  ## - Done
