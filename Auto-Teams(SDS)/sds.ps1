24b0a09e766ef41b1e0b5394e234e1055ec57cac
JoshTickles <joshangel@hotmail.co.uk>
2020-03-02 10:41:51 +1300

    Update sds.ps1

--- Auto-Teams(SDS)/sds.ps1
+++ Auto-Teams(SDS)/sds.ps1
@@ -15,12 +15,12 @@ Change Log
 
 $date = (Get-date).Day
 
-# Get JSON file where file size is greater than 1 meg and is today's Date and copy to upload folder
+# Get JSON file where file size is greater than 1 meg and is today's Date and copy to upload folder. Change size as required for your client site. 
 
-Get-ChildItem .\dsdata\*.json | where { $_.Length -gt 100KB} | Where { $_.LastwriteTime.Day -eq "$date"} | Copy-Item -Destination .\Upload -force
+Get-ChildItem .\dsdata\*.json | where { $_.Length -gt 1000KB} | Where { $_.LastwriteTime.Day -eq "$date"} | Copy-Item -Destination .\Upload -force
 
 # Execute Teams Export Script
 
-& '.\teamsexport_sm.ps1'
+& '.\teamsexport.ps1'
 
 & '.\zip-email.ps1'
